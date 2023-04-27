0c64d744011d30dcc32bdaf2e1334dbfc86a0364
mikzanco <mikyzancolo97@gmial.com>
2023-04-27 17:57:48 +0200

    fix button reset

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const grid = document.getElementById("grid");
 let lockGame = false;
 // Set test mode to true if you want see mines location
-const testMode = false;
+const testMode = true;
 generateGrid();
 
 // Generare una griglia 10 *10
